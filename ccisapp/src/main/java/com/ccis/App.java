3077a0433200e660950fee4cd599e457c8001315
elmahdi <elmhadrielmahdi@gmail.com>
2025-04-17 11:04:51 +0100

    intializing the project

---
+++ ccisapp/src/main/java/com/ccis/App.java
@@ -0,0 +1,13 @@
+package com.ccis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
